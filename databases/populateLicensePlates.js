40ee1918461f8199702f2f38cc277970be9af409
Megan Honig <meganhonig@gmail.com>
2018-03-26 11:01:56 -0400

    add combinatorics to generate license plates

--- databases/populateLicensePlates.js
+++ databases/populateLicensePlates.js
@@ -1,36 +1,17 @@
 const fs = require('fs');
-const datamuse = require('datamuse');
 const mongoose = require('mongoose');
 const LicensePlate = require('../models/LicensePlate');
 const Word = require('../models/Word');
+const Combinatorics = require('js-combinatorics');
 
 
 mongoose.connect('mongodb://127.0.0.1/lpg', {poolSize: 10});
 
-const words = Word.find((err, data) => {
 
-});
 
-//word list will need to be pulled from DB before we continue.
+const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+let licensePlates = Combinatorics.baseN(alphabet, 3).toArray();
 
-// const generateLicensePlateList = () => {
-//   let licensePlates = [];
-//   let alphabet = 'abcdefghijklmnopqrstuvwxyz';
-//   for (let i = 0; i < alphabet.length; i++){
-//     let firstLetter = alphabet[i];
-//     for (let j = 0; i < alphabet.length; i++){
-//       let firstAndSecondLetter = firstLetter + alphabet[j];
-//       for (let k = 0; k < alphabet.length; k++){
-//         let allThreeLetters = firstAndSecondLetter + alphabet[k];
-//         licensePlates.push(allThreeLetters); 
-//       }
-//     }
-//   }
-//   return licensePlates;
-// };
+// const words = Word.find((err, data) => {
 
-// let licensePlateList = generateLicensePlateList();
-// console.log(licensePlateList);
-
-//use combinatorics package for this.
-
+// });
